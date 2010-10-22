1b932538061fb18335424d23f24d9f553e38cc48
rodney.amato <rodney.amato@interspire.com>
2010-10-22 14:15:15 +1100

    Change the way all ips are collected to ensure that only ips that are available for use are included. Ips reserved for future use will not show up in the list

--- getallips.php
+++ getallips.php
@@ -1,26 +1,30 @@
 <?php
 
 /**
-* Get a list of all the public ips in an account.
-* The list may contain duplicates
+* Get a list of all the public ips in an account that are routable.
 **/
 
 require_once dirname(__FILE__).'/common.php';
 
 $client = SoftLayer_SoapClient::getClient('SoftLayer_Account', null, Config::USERNAME, Config::KEY);
 
-$ips = array();
+$objectMask = new SoftLayer_ObjectMask();
+$objectMask->hardware;
+$objectMask->hardware->networkComponents;
+$objectMask->hardware->networkComponents->subnets;
+$objectMask->hardware->networkComponents->subnets->ipAddresses;
 
 try {
-    $servers = $client->getHardware();
-	foreach ($servers as $server) {
-		if (empty($server->primaryIpAddress)) {
-			continue;
-		}
-		$cidr = $server->primaryIpAddress."/32";
-		$ipEnum = new IpEnumerator($cidr);
-		foreach ($ipEnum as $ip) {
-			echo "$ip\n";
+	$client->setObjectMask($objectMask);
+	$data = $client->getObject();
+
+	foreach ($data->hardware as $server) {
+		foreach ($server->networkComponents as $components) {
+			foreach ($components->subnets as $subnet) {
+				foreach ($subnet->ipAddresses as $ipaddr) {
+					echo $ipaddr->ipAddress."\n";
+				}
+			}
 		}
 	}
 
@@ -29,8 +33,17 @@ try {
 	foreach ($publicNetworks as $network) {
 		$cidr = $network->networkIdentifier.'/'.$network->cidr;
 		$ipEnum = new IpEnumerator($cidr);
+		if ($network->subnetType != 'SECONDARY_ON_VLAN') {
+			continue;
+		}
+
 		foreach ($ipEnum as $ip) {
-			echo "$ip\n";
+			if (in_array($network->subnetType, array('PRIMARY', 'ADDITIONAL_PRIMARY', 'SECONDARY_ON_VLAN'))) {
+				if ($ip == $network->networkIdentifier || $ip == $network->broadcastAddress || $ip == $network->gateway) {
+					continue;
+				}
+			}
+			echo $ip."\n";
 		}
 	}
 } catch (Exception $e) {
