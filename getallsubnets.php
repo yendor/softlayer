e82eb50a3f1bd3ad8c0769e5cdcfa24bc098ac69
rodney.amato <rodney.amato@interspire.com>
2010-10-27 10:15:11 +1100

    We dont need to get the primary ip of any of the machines specifically since getPublicSubnets gets them all anyway

--- getallsubnets.php
+++ getallsubnets.php
@@ -5,15 +5,6 @@ require_once(dirname(__FILE__).'/common.php');
 $client = SoftLayer_SoapClient::getClient('SoftLayer_Account', null, Config::USERNAME, Config::KEY);
 
 try {
-    $servers = $client->getHardware();
-	foreach ($servers as $server) {
-		if (empty($server->primaryIpAddress)) {
-			continue;
-		}
-		$cidr = $server->primaryIpAddress."/32";
-		echo "$cidr\n";
-	}
-
 	$publicNetworks = $client->getPublicSubnets();
 
 	foreach ($publicNetworks as $network) {
